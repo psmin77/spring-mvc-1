71814ccbb3a44ac75b356af3a9fb496535242555
Seongmin <chr92@naver.com>
2022-11-05 11:00:43 +0900

    Section6-9-updated

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -2,6 +2,8 @@ package hello.springmvc.basic.request;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
+import java.util.stream.Stream;
 
 @Slf4j
 @Controller
@@ -30,12 +33,12 @@ public class RequestBodyStringController {
     }
 
     /**
-     * InputStream(Reader): HTTP 요청 메시지 바디의 내용을 직접 조회
-     * * OutputStream(Writer): HTTP 응답 메시지의 바디에 직접 결과 출력
-     * */
+     * InputStream(Reader) : HTTP 요청 메시지 바디의 내용을 직접 조회
+     * OutputStream(Writer) : HTTP 응답 메시지 바디에 직접 결과 출력
+     **/
     @PostMapping("/request-body-string-v2")
-    public void requestBodyStringV2(InputStream inputStream, Writer responseWriter)
-            throws IOException {
+    public void requestBodyStringV2(InputStream inputStream, Writer responseWriter) throws IOException {
+
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
         log.info("messageBody={}", messageBody);
         responseWriter.write("ok");
@@ -45,16 +48,18 @@ public class RequestBodyStringController {
      * HttpEntity: HTTP header, body 정보를 편리하게 조회
      * - 메시지 바디 정보를 직접 조회(@RequestParam X, @ModelAttribute X)
      * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
-     * *
+     *
      * 응답에서도 HttpEntity 사용 가능
      * - 메시지 바디 정보 직접 반환(view 조회X)
      * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
      */
     @PostMapping("/request-body-string-v3")
-    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) {
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) throws IOException {
+
         String messageBody = httpEntity.getBody();
         log.info("messageBody={}", messageBody);
-        return new HttpEntity<>("ok");
+
+        return new ResponseEntity<>("ok", HttpStatus.CREATED);
     }
 
     /**
@@ -70,5 +75,4 @@ public class RequestBodyStringController {
         log.info("messageBody={}", messageBody);
         return "ok";
     }
-
 }
